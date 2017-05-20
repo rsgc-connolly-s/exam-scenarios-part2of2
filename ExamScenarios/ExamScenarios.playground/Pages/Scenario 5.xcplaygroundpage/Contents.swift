d64688bed403c6226d2a0da92b23c3e41efaff15
Sean <student@macbook-pro-405-connolly.local>
2017-05-20 16:49:59 -0400

    finished scenario 5

--- ExamScenarios/ExamScenarios.playground/Pages/Scenario 5.xcplaygroundpage/Contents.swift
+++ ExamScenarios/ExamScenarios.playground/Pages/Scenario 5.xcplaygroundpage/Contents.swift
@@ -23,8 +23,59 @@ import PlaygroundSupport
 // Create a new canvas
 let canvas = Canvas(width: 400, height: 600)
 
-// Your code below...
+//Create Background
+canvas.drawShapesWithBorders = false
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 400, height: 600)
 
+//Draw arrow function
+func drawArrow(X: Int, Y: Int){
+    canvas.translate(byX: X, byY: Y)
+    
+    canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 15, height: 45)
+    canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 45, height: 15)
+    
+    canvas.rotate(by: 45)
+    canvas.drawRectangle(bottomLeftX: 20, bottomLeftY: -8, width: 55, height: 15)
+    canvas.rotate(by: -45)
+    
+    canvas.translate(byX: -X, byY: -Y)
+}
+
+
+for x in stride(from: 0, through: 335, by: 67){
+    for y in stride(from: 0, through: 335, by: 67){
+        
+        if x == y {
+            canvas.fillColor = Color.white
+            drawArrow(X: x, Y: y)
+        }else{
+            canvas.fillColor = Color.init(hue: 313, saturation: 77, brightness: 65, alpha: 100)
+            drawArrow(X: x, Y: y)
+        }
+       
+    }
+}
+
+
+//draw lines 
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 440, width: 450, height: 2)
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 560, width: 400, height: 2)
+
+//Title
+canvas.textColor = Color.white
+canvas.drawText(message: "the velvet underground", size: 30, x: 15, y: 400)
+
+//Draw top left info
+canvas.drawText(message: "first appearance in london", size: 9, x: 9, y: 535)
+canvas.drawText(message: "the london college of printing", size: 9, x: 10, y: 525)
+
+//Draw the middle info
+canvas.drawText(message: "with spring and pollyfloskin", size: 9, x: 150, y: 535)
+canvas.drawText(message: "plus the great western light show", size: 9, x: 150, y: 525)
+
+//Draw top right info
+canvas.drawText(message: "thursday", size: 9, x: 300, y: 535)
+canvas.drawText(message: "october 14 1971/8pm", size: 9, x: 300, y: 525)
 //: ## Template code
 //: The code below is necessary to see the result of your work in the Assistant Editor at right. Please do not remove.
 PlaygroundPage.current.liveView = canvas.imageView
